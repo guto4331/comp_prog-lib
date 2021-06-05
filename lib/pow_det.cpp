8fec688d0b13b203bb207e4661dce72da471e6da
zuzuzuzu4z <suzutaisei1223@gmail.com>
2021-06-05 20:25:42 +0900

    変数の名前を修正

--- lib/pow_det.cpp
+++ lib/pow_det.cpp
@@ -1,15 +1,17 @@
+//行列の累乗を O(m^2 log n) で、mint で求める
+
 vector<vector<mint>> pow_det(vector<vector<mint>>& base, vector<vector<mint>> a, ll n){
     if(n == 1) return a;
-    int b = a.size();
+    int m = a.size();
     if(n % 2 == 1){
         vector temp = pow_det(base, a, n - 1);
-        vector ret(b, vector(b, mint(0)));
-        rep(i, 0, b) rep(j, 0, b) rep(k, 0, b) ret[i][j] += temp[i][k] * base[k][j];
+        vector ret(m, vector(m, mint(0)));
+        rep(i, 0, m) rep(j, 0, m) rep(k, 0, m) ret[i][j] += temp[i][k] * base[k][j];
         return ret;
     }else{
         vector temp = pow_det(base, a, n / 2);
-        vector ret(b, vector(b, mint(0)));
-        rep(i, 0, b) rep(j, 0, b) rep(k, 0, b) ret[i][j] += temp[i][k] * temp[k][j];
+        vector ret(m, vector(m, mint(0)));
+        rep(i, 0, m) rep(j, 0, m) rep(k, 0, m) ret[i][j] += temp[i][k] * temp[k][j];
         return ret;
     }
 }
