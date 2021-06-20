ce48e84eedd5caf99da57482999a1733e2068a21
zuzuzuzu4z <unconfigured@null.spigotmc.org>
2021-06-20 12:05:15 +0900

    無駄な改行を削除

--- geometry.cpp
+++ geometry.cpp
@@ -11,59 +11,31 @@ namespace geometry2d{
     struct Point{
         double x, y;
         Point(double x = 0.0, double y = 0.0) : x(x), y(y) {};
-        bool operator ==(Point other) const{
-            return (x == other.x && y == other.y);
-        }
-        bool operator <(Point other){
-            if(sgn(x - other.x) == 0) return sgn(y - other.y) < 0;
-            else return sgn(x - other.x) < 0;
-        }
+        bool operator ==(Point other) const{ return (x == other.x && y == other.y); }
+        bool operator <(Point other){ if(sgn(x - other.x) == 0) return sgn(y - other.y) < 0; else return sgn(x - other.x) < 0; }
         bool operator <=(Point other){ return (*this == other || *this < other); }
         bool operator >(Point other){ return !(*this <= other); }
         bool operator >=(Point other){ return !(*this < other); }
         bool operator !=(Point other) const{ return !(*this == other); }
         Point operator +() const{ return *this; }
         Point operator +(const Point& other) const{ return { x + other.x, y + other.y }; }
-        Point& operator +=(const Point& other){
-            x += other.x;
-            y += other.x;
-            return *this;
-        }
+        Point& operator +=(const Point& other){ x += other.x; y += other.x; return *this; }
         Point operator -() const{ return { -x, -y }; }
         Point operator -(const Point& other) const{ return { x - other.x, y - other.y }; }
-        Point& operator -=(const Point& other){
-            x -= other.x;
-            y -= other.x;
-            return *this;
-        }
+        Point& operator -=(const Point& other){ x -= other.x; y -= other.x; return *this; }
         Point operator *(double k) const{ return { x * k, y * k }; }
-        Point& operator *=(double k){
-            x *= k;
-            y *= k;
-            return *this;
-        }
+        Point& operator *=(double k){ x *= k; y *= k; return *this; }
         Point operator /(double k) const{ return { x / k, y / k }; }
-        Point& operator /=(double k){
-            x /= k;
-            y /= k;
-            return *this;
-        }
+        Point& operator /=(double k){ x /= k; y /= k; return *this; }
         double length() const{ return sqrt(x * x + y * y); }
         double sqLength() const{ return x * x + y * y; }
-        double dist(Point other) const{
-            double dx = x - other.x;
-            double dy = y - other.y;
-            return sqrt(dx * dx + dy * dy);
-        }
+        double dist(Point other) const{ double dx = x - other.x; double dy = y - other.y; return sqrt(dx * dx + dy * dy); }
         bool isZero(){ return (x == 0 && y == 0); }
         double dot(Point other) const{ return x * other.x + y * other.y; }
         double cross(Point other) const{ return x * other.y - y * other.x; }
         Point normalized() const{ return { x / length(), y / length() }; }
         Point normalUnitVector() const{ return { -normalized().y, normalized().x }; }
-        Point rotation(double arg) const{
-            double cs = cos(arg), sn = sin(arg);
-            return { x * cs - y * sn, x * sn + y * cs };
-        }
+        Point rotation(double arg) const{ double cs = cos(arg), sn = sin(arg); return { x * cs - y * sn, x * sn + y * cs }; }
     };
     const Point error_val = {254183.0, -595361.0};
     double dot(Point a, Point b){ return a.x * b.x + a.y * b.y; }
@@ -86,49 +58,35 @@ namespace geometry2d{
     struct Line{
         Point from, to;
         Line(Point from, Point to) : from(from), to(to) {} ;
-        Point toVec() const{
-            return to - from;
-        }
+        Point toVec() const{ return to - from; }
     };
     typedef Line Ray;
     typedef Line Segment;
     pair<bool, Point> lineIntersection(Line a, Line b){
         if(sgn(a.toVec().cross(b.toVec())) == 0) return { false, error_val };
-        Point ret = 
-            a.from + b.toVec() * abs((b.to - a.from).cross(b.toVec()) / a.toVec().cross(b.toVec()));
+        Point ret = a.from + b.toVec() * abs((b.to - a.from).cross(b.toVec()) / a.toVec().cross(b.toVec()));
         return { true, ret };
     }
     pair<bool, Point> segmentIntersection(Segment a, Segment b){
-        if(iSP(a.from, a.to, b.from) * iSP(a.from, a.to, b.to) <= 0
-            && iSP(b.from, b.to, a.from) * iSP(b.from, b.to, a.to) <= 0){
+        if(iSP(a.from, a.to, b.from) * iSP(a.from, a.to, b.to) <= 0 && iSP(b.from, b.to, a.from) * iSP(b.from, b.to, a.to) <= 0){
             if(a.toVec().cross(b.toVec()) == 0) return { true, error_val };
             else return lineIntersection(a, b);
         }else return { false, error_val };
     }
-    double dist(Point from, Point to){
-        double dx = to.x - from.x;
-        double dy = to.y - from.y;
-        return sqrt(dx * dx + dy * dy);
-    }
+    double dist(Point from, Point to){ double dx = to.x - from.x; double dy = to.y - from.y; return sqrt(dx * dx + dy * dy); }
     double distBetweenPointAndLine(Point p, Line l){ return abs(l.toVec().cross(p - l.from) / l.toVec().length()); }
     double distBetweenPointAndRay(Point p, Ray r){
         if(sgn((p - r.from).dot(r.toVec())) < 0) return r.from.dist(p);
         else return abs(r.toVec().cross(p - r.from) / r.toVec().length());
     }
     double distBetweenPointAndSegment(Point p, Segment s){
-        if(sgn(s.toVec().dot(p - s.from)) < 0 || sgn((-s.toVec()).dot(p - s.to)) < 0){
-            return min(p.dist(s.from), p.dist(s.to));
-        }else return distBetweenPointAndLine(p, s);
+        if(sgn(s.toVec().dot(p - s.from)) < 0 || sgn((-s.toVec()).dot(p - s.to)) < 0) return min(p.dist(s.from), p.dist(s.to));
+        else return distBetweenPointAndLine(p, s);
     }
     Point projection(Point a, Line l){
-        Point ret =
-            l.from + l.toVec().normalized() * (a - l.from).dot(l.toVec()) / l.toVec().length();
+        Point ret = l.from + l.toVec().normalized() * (a - l.from).dot(l.toVec()) / l.toVec().length();
         return ret;
     }
-    Point reflection(Point a, Line l){
-        Point ret = 
-            a + (projection(a, l) - a) * 2;
-        return ret;
-    }
+    Point reflection(Point a, Line l){ Point ret = a + (projection(a, l) - a) * 2; return ret; }
 }
 using namespace geometry2d;
