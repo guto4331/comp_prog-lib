45d7709eaca345604a2aa994ae3631957e050273
zuzuzuzu4z <unconfigured@null.spigotmc.org>
2021-07-17 19:13:23 +0900

    first commit

---
+++ lib/nCk.cpp
@@ -0,0 +1,23 @@
+// nCk (mod MOD) 前処理 COMinit(): O(n), クエリ COM(): O(1)
+
+const int MAX = 510000;
+const int MOD = 1000000007;
+
+ll fac[MAX], finv[MAX], inv[MAX];
+
+void COMinit() {
+    fac[0] = fac[1] = 1;
+    finv[0] = finv[1] = 1;
+    inv[1] = 1;
+    for (int i = 2; i < MAX; i++){
+        fac[i] = fac[i - 1] * i % MOD;
+        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+        finv[i] = finv[i - 1] * inv[i] % MOD;
+    }
+}
+
+ll COM(int n, int k){
+    if (n < k) return 0;
+    if (n < 0 || k < 0) return 0;
+    return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
