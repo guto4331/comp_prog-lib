df472d86fdb37186cc75eb7a0501eebae3bb4afa
zuzuzuzu4z <unconfigured@null.spigotmc.org>
2021-08-08 23:06:48 +0900

    first commit

---
+++ lib/boost_include.cpp
@@ -0,0 +1,5 @@
+#include <boost/multiprecision/cpp_dec_float.hpp>
+#include <boost/multiprecision/cpp_int.hpp>
+namespace mp = boost::multiprecision;
+using Bint = mp::cpp_int;
+using d32 = mp::number<mp::cpp_dec_float<32>>;
