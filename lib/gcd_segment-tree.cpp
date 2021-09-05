c7261952525f5bb5d136db468d8d0d196ae504bf
zuzuzuzu4z <68155628+zuzuzuzu4z@users.noreply.github.com>
2021-09-05 15:12:22 +0900

    Delete gcd_segment-tree.cpp

--- lib/gcd_segment-tree.cpp
+++
@@ -1,9 +0,0 @@
-// サイズ n の配列 a について、任意の区間の最大公約数を求める。 O(n log n)
-
-int op(int a, int b) { return gcd(a, b); }
-int e(){ return 0; }
-
-segtree<int, op, e> seg(n);
-rep(i, 0, n) seg.set(i, a[i]);
-
-// seg.prod(l, r) で gcd(a[l], a[l + 1], ... , a[r - 1]) を求める
