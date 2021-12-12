5ef0817da8f26105e95ffece15350ef4dfc7024f
zuzuzuzu4z <68155628+zuzuzuzu4z@users.noreply.github.com>
2021-12-12 10:09:06 +0900

    first commit

---
+++ lib/compress.cpp
@@ -0,0 +1,10 @@
+// 座標圧縮 O(n log n)
+template<class T> vector<T> compress(const vector<T>& a){
+    int n = a.size();
+    vector<T> b = a;
+    sort(all(b));
+    b.erase(unique(all(b)), b.end());
+    vector<T> ret(n);
+    rep(i, 0, n) ret[i] = lower_bound(all(b), a[i]) - b.begin();
+    return ret;
+}
