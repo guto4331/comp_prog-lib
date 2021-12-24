83c8882fa9237bd5ac65c7bd3f60d7aa444fe800
zuzuzuzu4z <68155628+zuzuzuzu4z@users.noreply.github.com>
2021-12-24 20:17:15 +0900

    changed file name

--- lib/pow_mat.cpp
+++ lib/pow_mat.cpp
@@ -1,15 +1,15 @@
 // m x m 行列の n 乗を O(m^3 log n) で、mint で求める
 
-vector<vector<mint>> pow_det(const vector<vector<mint>>& base, vector<vector<mint>> a, ll n){
+vector<vector<mint>> pow_mat(const vector<vector<mint>>& base, vector<vector<mint>> a, ll n){
     if(n == 1) return a;
     int m = a.size();
     if(n % 2 == 1){
-        vector temp = pow_det(base, a, n - 1);
+        vector temp = pow_mat(base, a, n - 1);
         vector ret(m, vector(m, mint(0)));
         rep(i, 0, m) rep(j, 0, m) rep(k, 0, m) ret[i][j] += temp[i][k] * base[k][j];
         return ret;
     }else{
-        vector temp = pow_det(base, a, n / 2);
+        vector temp = pow_mat(base, a, n / 2);
         vector ret(m, vector(m, mint(0)));
         rep(i, 0, m) rep(j, 0, m) rep(k, 0, m) ret[i][j] += temp[i][k] * temp[k][j];
         return ret;
