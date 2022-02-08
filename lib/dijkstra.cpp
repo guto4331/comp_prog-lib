ac9b379464f7b32406f24095556abecb6fe6f39d
zuzuzuzu4z <68155628+zuzuzuzu4z@users.noreply.github.com>
2022-02-08 21:01:39 +0900

    拡張ダイクストラを追加

--- lib/dijkstra.cpp
+++ lib/dijkstra.cpp
@@ -16,3 +16,29 @@ vector<ll> dijkstra(const Graph &g, int s){
     }
     return dist;
 }
+
+// 拡張ダイクストラ　情報を多くもつ必要があるとき
+
+typedef struct info{
+    ll dist; int from, cur;
+    info(ll dist, int from, int cur) : dist(dist), from(from), cur(cur) {}
+}info_t;
+
+vector dist(n, vector(n, INF * INF));
+priority_queue<info_t, vector<info_t>, greater<info_t>> q;
+rep(i, 0, n){
+    dist[i][0] = 0;
+}
+q.emplace(0, -1, 0);
+while(!q.empty()){
+    auto [c, prev, cur] = q.top(); q.pop();
+    if(prev != -1 && dist[prev][cur] < c) continue;
+    for(const auto& e : g[cur]){
+        if(prev != -1 && /* 条件 */) continue;
+        if(chmin(dist[cur][e.to], c + e.cost)) q.emplace(dist[cur][e.to], cur, e.to);
+    }
+}
+
+ll ans = INF * INF;
+rep(i, 0, n) chmin(ans, dist[i][1]);
+cout << (ans == INF * INF ? -1 : ans) << endl;
